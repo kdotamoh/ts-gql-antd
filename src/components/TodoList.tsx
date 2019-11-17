3f4b61bc647b1dc61e28f14c61df6b36e5c67476
kdotamoh <kwadwo.amoh@gmail.com>
2019-11-17 22:40:41 +0000

    Show loading spinner

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Link } from '@reach/router';
 import { useQuery } from '@apollo/react-hooks';
-import { List } from 'antd';
+import { List, Spin } from 'antd';
 
 import Todo from 'components/Todo';
 import { GET_TODOS } from 'queries';
@@ -9,24 +9,32 @@ import { GET_TODOS } from 'queries';
 const TodoList: React.FC = () => {
   const { data, loading, error } = useQuery(GET_TODOS);
 
-  if (loading) return <div>Loading...</div>;
   if (error) return <p>ERROR: {error.message}</p>;
 
   return (
     <div>
-      <List
-        locale={{ emptyText: "You haven't added any todos." }}
-        dataSource={data.todos}
-        itemLayout="horizontal"
-        renderItem={(todo: { id: string; type: string }): ReactNode => (
-          <List.Item style={{ justifyContent: 'space-between' }} key={todo.id}>
-            <Todo id={todo.id} type={todo.type} />
-            <Link style={{ float: 'right' }} to={`/todo/${todo.id}`}>
-              View
-            </Link>
-          </List.Item>
-        )}
-      />
+      {loading ? (
+        <div className="loading-container">
+          <Spin />
+        </div>
+      ) : (
+        <List
+          locale={{ emptyText: "You haven't added any todos." }}
+          dataSource={data.todos}
+          itemLayout="horizontal"
+          renderItem={(todo: { id: string; type: string }): ReactNode => (
+            <List.Item
+              style={{ justifyContent: 'space-between' }}
+              key={todo.id}
+            >
+              <Todo id={todo.id} type={todo.type} />
+              <Link style={{ float: 'right' }} to={`/todo/${todo.id}`}>
+                View
+              </Link>
+            </List.Item>
+          )}
+        />
+      )}
     </div>
   );
 };
--- src/screens/ViewTodo.tsx
+++ src/screens/ViewTodo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { useQuery } from '@apollo/react-hooks';
-import { Card } from 'antd';
+import { Card, Spin } from 'antd';
 
 import { GET_TODO } from 'queries';
 import Todo from 'components/Todo';
@@ -12,14 +12,21 @@ export type Props = RouteComponentProps<{ id: string }>;
 const ViewTodo: React.FC<Props> = ({ id }) => {
   const { data, loading, error } = useQuery(GET_TODO, { variables: { id } });
 
-  if (loading) return <div>Loading...</div>;
   if (error) return <p>ERROR: {error.message}</p>;
 
   return (
     <Card title="View todo">
-      <Todo id={data.todo.id} type={data.todo.type} />
-      <div className="mb-3rem" />
-      <AddTodo isEditing={true} id={data.todo.id} />
+      {loading ? (
+        <div className="loading-container">
+          <Spin />
+        </div>
+      ) : (
+        <React.Fragment>
+          <Todo id={data.todo.id} type={data.todo.type} />
+          <div className="mb-3rem" />
+          <AddTodo isEditing={true} id={data.todo.id} />
+        </React.Fragment>
+      )}
     </Card>
   );
 };
